69a9f3e875391a563251ad8cd0e522b2443dbd22
Jake Simms <jakedsimms@gmail.com>
2014-04-13 22:20:43 -0400

    a function that determines if a number is prime. for a gist a day

---
+++ is_prime?.rb
@@ -0,0 +1,24 @@
+#Write a method, is_prime?, that takes a number num and returns true if it is prime and false otherwise.
+
+def is_prime?(num)
+	i = 1  
+	while i <= num 
+		if num == 2 
+			return true  
+		elsif i == 1 || i == num 
+			i += 1 
+		elsif num%i == 0
+			return false
+			exit    	  
+		else 
+			return true
+			i += 1   
+		end
+	end 
+end    
+		  
+
+puts is_prime?(24) 
+
+
+
