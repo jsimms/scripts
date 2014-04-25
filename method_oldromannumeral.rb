e11160c1ab2a79900524cfc7c105791f606aff80
Jake Simms <jakedsimms@gmail.com>
2014-04-24 23:23:36 -0700

    old style romans. accordig to pine.

--- method_oldromannumeral.rb
+++ method_oldromannumeral.rb
@@ -1,4 +1,4 @@
-# NOT DONE, KEEP ON DOIN' 
+# NOT DONE, KEEP ON DOIN'
 
 =begin
 
@@ -16,14 +16,40 @@
 
 def old_roman_numeral(number)
   numeral = []
-  remaining = number
-  while remaining != 0
-    if number % 1000 == 0
-      numeral << "M"*(number/1000)
+  while number != 0
 
-    end
+    m = number/1000
+    numeral << "M"*m
+    number = number%1000
+
+    d = number/500
+    numeral << "D"*d
+    number = number%500
+
+    c = number/100
+    numeral << "C"*c
+    number = number%100
+
+    l = number/50
+    numeral << "L"*l
+    number = number%50
+
+    x = number/10
+    numeral << "X"*x
+    number = number%10
+
+    v = number/5
+    numeral << "V"*v
+    number = number%5
+
+    i = number/1
+    numeral << "I"*i
+    number = number%1
+
+    return numeral.join.to_s
   end
-  numeral
+
+
 end
 
-puts old_roman_numeral(4400)
+puts old_roman_numeral(9931)
