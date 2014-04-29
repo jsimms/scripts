955ddd6466801df5443b15fd76250ee9b2b14235
Jake Simms <jakedsimms@gmail.com>
2014-04-29 17:06:22 -0400

    cheated and looked it up

--- program_wordsortwithoutsort.rb
+++ program_wordsortwithoutsort.rb
@@ -1,24 +1,37 @@
-#WIP
+# Taken from Chris Pine's Learn to Program sample problems.
 
-# Take an array of words and sort it without using Ruby's sort method.
+def sort(ary)
+  # By building a wrapper method, we make it so we don't have to provide an
+  # empty array everytime we call sort.
+  rec_sort ary, []
+end
 
-def sort(sort_me)
-  sorted = []
-
-  # Loop through the unsorted array and find the 'shortest' word
-  shortest = sort_me[0]
-  sort_me.each do |x|
-    if x < shortest
-      shortest = x
+def rec_sort(unsorted, sorted)
+  # If there's nothing to sort, just return the empty array
+  if unsorted.length <= 0
+    return sorted
+  end
+  # If ther array contains elements, get to sorting.
+  smallest = unsorted.pop
+  still_unsorted = []
+  unsorted.each do |x|
+    if x < smallest
+      still_unsorted.push smallest
+      smallest = x
+    else
+      still_unsorted.push x
     end
   end
 
-  # Push the 'shortest' word into a new 'sorted' array
-  sorted << shortest
+  # Now, we've looped through all the elements of the array, and smallest
+  # really will be the smallest. Everything else is still_unsorted.
 
-  # Remove shortest from the usorted array
-  # ? ?
-  
+  # Now, put the smallest into the sorted array.
+  sorted.push smallest
+
+  # And then do it all over again with the new 'unsorted' array that no longer
+  # contains the shortest element from the previous 'unsorted' array
+  rec_sort(still_unsorted, sorted)
 end
 
 puts sort(['this', 'is', 'a', 'weird', 'unsorted', 'array', 'totally', 'random'])
