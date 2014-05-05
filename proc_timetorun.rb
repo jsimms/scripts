2e8d11f0f5a371b1a523067a4188d294e03b08b0
Jake Simms <jakedsimms@gmail.com>
2014-05-05 13:28:57 -0400

    a quick snip that determines how long it takes to run a lambda of code

---
+++ proc_timetorun.rb
@@ -0,0 +1,8 @@
+# Given some code in the form of lambdas.
+# Measure and return the time taken to execute that code.
+
+def exec_time(proc)
+  begin_time = Time.now
+  proc.call
+  Time.now - begin_time
+end
