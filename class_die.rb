d027e07b9f1c61f62c06bf5465b46f2db7ba33a4
Jake Simms <jakedsimms@gmail.com>
2014-05-21 17:05:37 -0400

    die example

--- class_die.rb
+++ class_die.rb
@@ -1,29 +1,30 @@
 #Create an example Dice class
 
 class Die
-  
+
   def initialize
-		roll 
-	end 
+		roll
+	end
 
 	def roll
     @numberShowing = 1 + rand(6)
   end
 
-	def showing 
-		@numberShowing 
-	end 
+	def showing
+		@numberShowing
+	end
 
 	def cheat n
 		if (n <= 6 and n >= 1)
-			@numberShowing = n 	
-		else 
+			@numberShowing = n
+		else
 			return "Yeah, no. Choose 1-6."
 		end
-	end  
+	end
 
-end 
+end
 
-
- 
-puts Die.new.cheat(3)
+dice = [Die.new, Die.new]
+dice.each do |die|
+  puts die.roll
+end
